3da9c0074653abb7615313ba5451d2e8436e072a
adriaboschmatas <s232775@dtu.dk>
2025-01-21 13:15:14 +0100

    Uploading train_cloud.py

---
+++ src/nrms_ml_ops/train_cloud.py
@@ -0,0 +1,167 @@
+from pathlib import Path
+import tensorflow as tf
+import datetime as dt
+import polars as pl
+import gc
+import os
+import numpy as np
+
+
+from utils._constants import (
+    DEFAULT_HISTORY_ARTICLE_ID_COL,
+    DEFAULT_CLICKED_ARTICLES_COL,
+    DEFAULT_INVIEW_ARTICLES_COL,
+    DEFAULT_IMPRESSION_ID_COL,
+    DEFAULT_USER_COL,
+)
+
+from utils._articles import create_article_id_to_value_mapping
+
+from dataloader import NRMSDataLoader
+from model_config import hparams_nrms
+from model import NRMSModel_docvec
+
+
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+tf.config.optimizer.set_jit(False)
+
+
+#PATH = Path("./data/processed").expanduser()
+cloud_path = "gs://project_mlops_bucket/data/processed/"
+DUMP_DIR = Path(cloud_path + "ebnerd_predictions")
+DUMP_DIR.mkdir(exist_ok=True, parents=True)
+DT_NOW = dt.datetime.now()
+
+gpus = tf.config.experimental.list_physical_devices("GPU")
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+
+EPOCHS = 10
+embedding = "xlm_roberta_base"
+BATCH_SIZE = 32
+learning_rate = 1e-4
+HISTORY_SIZE = 35
+
+MODEL_NAME = f"NRMS-{DT_NOW}"
+
+# Create the folder, including any intermediate directories
+
+MODEL_WEIGHTS = f"./models/{MODEL_NAME}"
+Path(MODEL_WEIGHTS).mkdir(parents=True, exist_ok=True)
+LOG_DIR = DUMP_DIR.joinpath(f"./runs/{MODEL_NAME}")
+
+COLUMNS = [
+    DEFAULT_USER_COL,
+    DEFAULT_HISTORY_ARTICLE_ID_COL,
+    DEFAULT_INVIEW_ARTICLES_COL,
+    DEFAULT_CLICKED_ARTICLES_COL,
+    DEFAULT_IMPRESSION_ID_COL,
+]
+
+df_train = pl.read_parquet(cloud_path+"train.parquet")
+
+
+df_validation = pl.read_parquet(cloud_path+"validation.parquet")
+
+
+df_articles = pl.read_parquet(cloud_path+"articles.parquet")
+
+precomputed_embeddings = pl.read_parquet(cloud_path + embedding + ".parquet")
+
+precomputed_embeddings = precomputed_embeddings.filter(
+    precomputed_embeddings["article_id"].is_in(df_articles["article_id"])
+)
+precomputed_embeddings = precomputed_embeddings.rename({"FacebookAI/xlm-roberta-base": "embedding"})
+
+pre_embs = np.array([precomputed_embeddings["embedding"][0]])
+
+article_mapping = create_article_id_to_value_mapping(
+    df=precomputed_embeddings,
+    value_col="embedding",  # Column containing precomputed embeddings
+    article_col="article_id",  # Column containing article IDs
+)
+
+
+train_dataloader = NRMSDataLoader(
+    behaviors=df_train,
+    article_dict=article_mapping,
+    unknown_representation="zeros",
+    history_column=DEFAULT_HISTORY_ARTICLE_ID_COL,
+    eval_mode=False,
+    batch_size=BATCH_SIZE,
+)
+val_dataloader = NRMSDataLoader(
+    behaviors=df_validation,
+    article_dict=article_mapping,
+    unknown_representation="zeros",
+    history_column=DEFAULT_HISTORY_ARTICLE_ID_COL,
+    eval_mode=False,
+    batch_size=BATCH_SIZE,
+)
+
+hparams_nrms.history_size = HISTORY_SIZE
+hparams_nrms.learning_rate = learning_rate
+hparams_nrms.title_size = pre_embs[0].shape[0]
+
+
+model = NRMSModel_docvec(
+    hparams=hparams_nrms,
+    seed=42,
+)
+
+model.model.compile(
+    optimizer=model.model.optimizer,
+    loss=model.model.loss,
+    metrics=["AUC"],
+)
+
+# CALLBACKS
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=LOG_DIR, histogram_freq=1)
+
+# Earlystopping:
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor="val_loss",
+    mode="max",
+    patience=3,
+    restore_best_weights=True,
+)
+
+# ModelCheckpoint:
+modelcheckpoint = tf.keras.callbacks.ModelCheckpoint(
+    filepath=MODEL_WEIGHTS + "nrms.weights.h5",
+    monitor="val_loss",
+    mode="max",
+    save_best_only=False,
+    save_weights_only=True,
+    verbose=1,
+)
+
+# Learning rate scheduler:
+lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(
+    monitor="val_loss",
+    mode="max",
+    factor=0.2,
+    patience=2,
+    min_lr=learning_rate,
+)
+
+hist = model.model.fit(
+    train_dataloader,
+    validation_data=val_dataloader,
+    epochs=EPOCHS,
+    callbacks=[tensorboard_callback, early_stopping, modelcheckpoint, lr_scheduler],
+)
+
+tr_loss = hist.history["loss"]
+val_loss = hist.history["val_loss"]
+losses = [tr_loss, val_loss]
+
+gc.collect()
+
+print("saving model...")
+model.model.save_weights(MODEL_WEIGHTS + "nrms.weights.h5")
+
+
+print("Correctly ended")
