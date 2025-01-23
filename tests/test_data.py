8639fa4b3de5868d175c6f2858db42a3953086b8
edgar <s242781@dtu.dk>
2025-01-23 13:19:29 +0100

    Test data pytest

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,10 @@
-from torch.utils.data import Dataset
+#from torch.utils.data import Dataset
 import polars as pl
 from tests import _PATH_DATA
 from pathlib import Path
 
-def test_my_dataset():
-    """Test the MyDataset class."""
+def test_dataset_dimensions():
+    """Test that the dataset has the correct dimensions."""
     path_data = Path(_PATH_DATA)
     train = pl.read_parquet(path_data.joinpath("train.parquet"))
     test = pl.read_parquet(path_data.joinpath("test.parquet"))
@@ -12,11 +12,30 @@ def test_my_dataset():
     assert len(train) == 3513
     assert len(test) == 2466
     assert len(validation) == 765
-    # for dataset in [train, test]:
-    #     for x, y in dataset:
-    #         assert x.shape == (1, 28, 28)
-    #         assert y in range(10)
-    # train_targets = torch.unique(train.tensors[1])
-    # assert (train_targets == torch.arange(0,10)).all()
-    # test_targets = torch.unique(test.tensors[1])
-    # assert (test_targets == torch.arange(0,10)).all()
+    for dataset in [train, test, validation]:
+        for sample in range(len(dataset)):
+            assert dataset[sample].shape == (1,6)
+            
+
+# write a test that checks that there are no missing values in the dataset
+
+def test_dataset_missing_values():
+    """Test that the dataset has no missing values."""
+    path_data = Path(_PATH_DATA)
+    train = pl.read_parquet(path_data.joinpath("train.parquet"))
+    test = pl.read_parquet(path_data.joinpath("test.parquet"))
+    validation = pl.read_parquet(path_data.joinpath("validation.parquet"))
+    assert train.null_count().sum_horizontal()[0] == 0
+    assert test.null_count().sum_horizontal()[0] == 0
+    assert validation.null_count().sum_horizontal()[0] == 0  
+    
+# write a test that checks that the dataset has the correct columns
+def test_dataset_columns():
+    """Test that the dataset has the correct columns."""
+    path_data = Path(_PATH_DATA)
+    train = pl.read_parquet(path_data.joinpath("train.parquet"))
+    test = pl.read_parquet(path_data.joinpath("test.parquet"))
+    validation = pl.read_parquet(path_data.joinpath("validation.parquet"))
+    assert train.columns == ['user_id', 'article_id_fixed', 'article_ids_inview', 'article_ids_clicked', 'impression_id', 'labels']
+    assert test.columns == ['user_id', 'article_id_fixed', 'article_ids_inview', 'article_ids_clicked', 'impression_id', 'labels']
+    assert validation.columns == ['user_id', 'article_id_fixed', 'article_ids_inview', 'article_ids_clicked', 'impression_id', 'labels']  
