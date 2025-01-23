52177f586e5249c73b117cfa6cd621bca2cf3803
Paula Gambus <paulagambus06@gmail.com>
2025-01-23 17:58:03 +0100

    the api works with the cloud test data

--- src/nrms_ml_ops/api.py
+++ src/nrms_ml_ops/api.py
@@ -1,38 +1,3 @@
-# from fastapi import FastAPI, UploadFile, File
-# from typing import Optional
-# from enum import Enum
-# from http import HTTPStatus
-# import cv2
-
-# app = FastAPI()
-
-# @app.get("/")
-# def root():
-#     return {"Hello": "World"}
-
-# class ItemEnum(Enum):
-#     alexnet = "alexnet"
-#     resnet = "resnet"
-#     lenet = "lenet"
-
-# @app.get("/restric_items/{item_id}")
-# def read_item(item_id: ItemEnum):
-#     return {"item_id": item_id}
-
-# @app.post("/cv_model/")
-# async def cv_model(data: UploadFile = File(...)):
-#     with open('image.jpg', 'wb') as image:
-#         content = await data.read()
-#         image.write(content)
-#         image.close()
-#     response = {
-#         "input": data,
-#         "message": HTTPStatus.OK.phrase,
-#         "status-code": HTTPStatus.OK,
-#     }
-#     return response
-
-
 from fastapi import FastAPI
 import subprocess
 import re
@@ -41,7 +6,7 @@ app = FastAPI()
 
 @app.post("/evaluate/")
 async def evaluate_model():
-    result = subprocess.run(["python", "evaluate.py"], capture_output=True, text=True)
+    result = subprocess.run(["python", "evaluate_cloud.py"], capture_output=True, text=True)
     # print("STDOUT:", result.stdout)
     # print("STDERR:", result.stderr)
     # Extract the last "Test AUC: " line with numbers from stdout
