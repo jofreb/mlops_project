c4769497107f75080670769adb119ed16600cda7
root <root@EdgarFabregatMSIP65.localdomain>
2025-01-23 13:19:29 +0100

    Test data pytest

---
+++ tests/test_model.py
@@ -0,0 +1,12 @@
+# write a test that checks that from a input dataset, the output dataset has the correct dimensions
+import polars as pl
+from tests import _PATH_DATA
+from pathlib import Path
+
+# def test_model_output_dimensions():
+#     """Test that the model output has the correct dimensions."""
+#     path_data = Path(_PATH_DATA)
+#     train = pl.read_parquet(path_data.joinpath("train.parquet"))
+#     x = train[0]
+#     y = model(x)
+#     assert y.shape == (1, 1)
