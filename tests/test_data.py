b8773f8b8e24ab645e6c20d240bc7b6f5f984f3f
edgar <s242781@dtu.dk>
2025-01-24 17:57:55 +0100

    Update test_data path

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,16 +2,21 @@
 # from torch.utils.data import Dataset
 
 import polars as pl
+import os
 from tests import _PATH_DATA
 from pathlib import Path
+import pytest
 
 
 def test_dataset_dimensions():
     """Test that the dataset has the correct dimensions."""
-    path_data = Path(_PATH_DATA)
-    train = pl.read_parquet(path_data.joinpath("train.parquet"))
-    test = pl.read_parquet(path_data.joinpath("test.parquet"))
-    validation = pl.read_parquet(path_data.joinpath("validation.parquet"))
+    #path_data = Path(_PATH_DATA)
+    
+    path_data = _PATH_DATA
+    train = pl.read_parquet(os.path.join(_PATH_DATA, "train.parquet"))
+    #train = pl.read_parquet(path_data.joinpath("train.parquet"))
+    test = pl.read_parquet(os.path.join(_PATH_DATA,"test.parquet"))
+    validation = pl.read_parquet(os.path.join(_PATH_DATA,"validation.parquet"))
     assert len(train) == 3513
     assert len(test) == 2466
     assert len(validation) == 765
@@ -22,26 +27,27 @@ def test_dataset_dimensions():
 
 # write a test that checks that there are no missing values in the dataset
 
-
+#@pytest.mark.skipif(not os.path.exists(os.path.file_path), reason="Data files not found")
 def test_dataset_missing_values():
     """Test that the dataset has no missing values."""
-    path_data = Path(_PATH_DATA)
-    train = pl.read_parquet(path_data.joinpath("train.parquet"))
-    test = pl.read_parquet(path_data.joinpath("test.parquet"))
-    validation = pl.read_parquet(path_data.joinpath("validation.parquet"))
+    #path_data = Path(_PATH_DATA)
+    train = pl.read_parquet(os.path.join(_PATH_DATA,"train.parquet"))
+    test = pl.read_parquet(os.path.join(_PATH_DATA,"test.parquet"))
+    validation = pl.read_parquet(os.path.join(_PATH_DATA,"validation.parquet"))
 
     assert train.null_count().sum_horizontal()[0] == 0
     assert test.null_count().sum_horizontal()[0] == 0
     assert validation.null_count().sum_horizontal()[0] == 0
 
 
-# write a test that checks that the dataset has the correct columns
+# Test that checks that the dataset has the correct columns
+#@pytest.mark.skipif(not os.path.exists(os.path.file_path), reason="Data files not found")
 def test_dataset_columns():
     """Test that the dataset has the correct columns."""
-    path_data = Path(_PATH_DATA)
-    train = pl.read_parquet(path_data.joinpath("train.parquet"))
-    test = pl.read_parquet(path_data.joinpath("test.parquet"))
-    validation = pl.read_parquet(path_data.joinpath("validation.parquet"))
+    #path_data = Path(_PATH_DATA)
+    train = pl.read_parquet(os.path.join(_PATH_DATA,"train.parquet"))
+    test = pl.read_parquet(os.path.join(_PATH_DATA,"test.parquet"))
+    validation = pl.read_parquet(os.path.join(_PATH_DATA,"validation.parquet"))
     assert train.columns == [
         "user_id",
         "article_id_fixed",
